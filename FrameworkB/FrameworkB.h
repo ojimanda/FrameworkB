c3d82a0e35f2cb1a89050a4a93706c2de15a9ac8
Yozi Reci Manda <YRManda@maybank.co.id>
2025-04-24 11:21:17 +0700

    Initial Commit

---
+++ FrameworkB/FrameworkB.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkB.h
+//  FrameworkB
+//
+//  Created by Yozi Reci Manda  on 24/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkB.
+FOUNDATION_EXPORT double FrameworkBVersionNumber;
+
+//! Project version string for FrameworkB.
+FOUNDATION_EXPORT const unsigned char FrameworkBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkB/PublicHeader.h>
+
+
